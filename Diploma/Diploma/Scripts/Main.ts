32ec609254223fdd70451756cfa4d0f7bdf991ae
Alek097 <petrovskii097@gmail.com>
2017-04-06 13:17:36 +0300

    fix static file, and added HomeController for return index.html

--- Diploma/Diploma/Scripts/Main.ts
+++ Diploma/Diploma/Scripts/Main.ts
@@ -1,1 +1,12 @@
-﻿
+﻿angular.module('main',
+    [
+        'ngRoute',
+        'ngAnimate',
+    ])
+    .config(($routeProvider: ng.route.IRouteProvider) => {
+        //$routeProvider.when('/Home',
+        //    <ng.route.IRoute>
+        //    {
+        //        templateUrl: '../Scripts/Pages/Home/HomeView.html',
+        //    });
+    });
