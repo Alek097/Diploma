49b1820ba9c571aba35669405675caa94ed848ab
Alek097 <petrovskii097@gmail.com>
2017-04-08 18:15:29 +0300

    Added style-loader, css-loader, less-loader. Transfer Style.less into Styles folder. Update bootsrap to v4.0.0-alpha.6. Added all js, css, html files to vendor.js

--- Diploma/Diploma/webpack.config.js
+++ Diploma/Diploma/webpack.config.js
@@ -7,11 +7,18 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        bundle: "./Scripts/Main.ts",
+        bundle: './Scripts/Main.ts',
+        vendor: [
+            './Bundles/lib/bootstrap/dist/js/bootstrap.min.js',
+            './Bundles/lib/angular/angular.min.js',
+            './Bundles/lib/angular-animate/angular-animate.min.js',
+            './Bundles/lib/angular-route/angular-route.min.js',
+            './Styles/Style.less'
+        ]
     },
     output: {
         path: __dirname + '/Bundles/',
-        filename: './app/bundle.js'
+        filename: './app/[name].js'
     },
     resolve: {
         extensions: ['.webpack.js', '.web.js', '.ts', '.js', 'tsx']
@@ -40,7 +47,19 @@ module.exports = {
                 test: /\.html$/,
                 exclude: /node_modules/,
                 loader: "html-loader?exportAsEs6Default"
-            }
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }
+                ]
+            },
         ]
     },
 
@@ -51,8 +70,8 @@ module.exports = {
             { from: 'node_modules/angular-animate', to: 'lib/angular-animate' },
             { from: 'node_modules/bootstrap', to: 'lib/bootstrap' },
             { from: 'node_modules/jquery', to: 'lib/jquery' },
-            { from: 'node_modules/less', to: 'lib/less' },
             { from: 'node_modules/font-awesome', to: 'lib/font-awesome' },
+            { from: 'node_modules/tether', to: 'lib/tether' }
         ])
     ]
 };
